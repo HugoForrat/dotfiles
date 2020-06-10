04f64ac2a18af01605e38d3dddd36bd341d035dc
Hugo <43822304+HugoForrat@users.noreply.github.com>
2020-06-10 18:17:09 +0200

    Silhouette of an install script

---
+++ auto_install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+ln -s /home/hugo/.dotfiles/vimrc ~/.vim/vimrc
+ln -s /home/hugo/.dotfiles/termite.conf /home/hugo/.config/termite/config
+ln -s /home/hugo/.dotfiles/i3config /home/hugo/.config/i3/config
+ln -s /home/hugo/.dotfiles/i3status.toml /home/hugo/.config/i3/status.toml
+ln -s /home/hugo/.dotfiles/alias /home/hugo/.alias
