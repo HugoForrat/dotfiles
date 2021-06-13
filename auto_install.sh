bac5d2e7fa0e22fde62822b10973ceccb0692735
Hugo Forrat <hugo.forrat@live.fr>
2021-06-13 13:40:42 +0200

    I wasn't going to use those.

--- auto_install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-ln -s $HOME/.dotfiles/vimrc ~/.vim/vimrc
-ln -s $HOME/.dotfiles/termite.conf $HOME/.config/termite/config
-ln -s $HOME/.dotfiles/i3config $HOME/.config/i3/config
-ln -s $HOME/.dotfiles/i3status.toml $HOME/.config/i3/status.toml
-ln -s $HOME/.dotfiles/alias $HOME/.alias
-ln -sf $HOME/.dotfiles/profile $HOME/.profile
